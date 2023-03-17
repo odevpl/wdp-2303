76013a6ef9a12bb5ba6cf2135ec3fed227be3d42
syltur <sylwia.tur99@gmail.com>
2023-03-17 18:59:17 +0100

    ADD funcionallity to toggleProductFavourite

--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -4,10 +4,27 @@ export const getCount = ({ products }) => products.length;
 
 export const getNew = ({ products }) =>
   products.filter(item => item.newFurniture === true);
+/* actions */
+const createActionName = actionName => `app/products/${actionName}`;
+const TOGGLE_PRODUCT_FAVOURITE = createActionName('TOGGLE_PRODUCT_FAVOURITE');
 
+/* action creators */
+export const toggleProductFavourite = payload => ({
+  type: TOGGLE_PRODUCT_FAVOURITE,
+  payload,
+});
 /* reducer */
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
+    case TOGGLE_PRODUCT_FAVOURITE:
+      return statePart.map(product =>
+        product.id === action.payload
+          ? {
+              ...product,
+              favourite: !product.favourite,
+            }
+          : product
+      );
     default:
       return statePart;
   }
