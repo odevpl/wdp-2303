ebeb2328a26d3b44be5658e538bf53b56c9b7ce9
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-03-22 14:44:49 +0100

    Add Banner component

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -6,9 +6,11 @@ import styles from './Homepage.module.scss';
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
 import Feedback from '../../features/Feedback/Feedback';
+import Banner from '../../common/Banner/Banner';
 
 const Homepage = () => (
   <div className={styles.root}>
+    <Banner />
     <FeatureBoxes />
     <NewFurniture />
     <Feedback />
