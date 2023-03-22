342ec59e6d3088d70b7be96b77bd0ba859c6d53e
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-03-22 11:38:42 +0100

    Add styles

--- src/components/common/ProductModal/ProductModal.js
+++ src/components/common/ProductModal/ProductModal.js
@@ -3,7 +3,8 @@ import Button from '../Button/Button';
 import PropTypes from 'prop-types';
 import styles from './ProductModal.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faXmark, faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
 
 const ProductModal = ({ closeModal, productData }) => {
   return (
@@ -14,10 +15,36 @@ const ProductModal = ({ closeModal, productData }) => {
             <FontAwesomeIcon icon={faXmark} />
           </Button>
         </div>
-        <img
-          alt={productData.name}
-          src={`${process.env.PUBLIC_URL}/images/furniture/${productData.category}/${productData.id}.jpg`}
-        />
+        <div className={`row ${styles.modalData}`}>
+          <div className={`col-6 ${styles.photo}`}>
+            <img
+              alt={productData.name}
+              src={`${process.env.PUBLIC_URL}/images/furniture/${productData.category}/${productData.id}.jpg`}
+            />
+          </div>
+          <div className={`col-6 ${styles.productParams}`}>
+            <div className={styles.title}>
+              <h5>{productData.name}</h5>
+            </div>
+            <div className={styles.stars}>
+              {[1, 2, 3, 4, 5].map(i => (
+                <a key={i} href='#'>
+                  {i <= productData.stars ? (
+                    <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
+                  ) : (
+                    <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
+                  )}
+                </a>
+              ))}
+            </div>
+            {productData.oldPrice && (
+              <div className={styles.oldPrice}>$ {productData.oldPrice}</div>
+            )}
+            <div className={styles.price}>
+              <Button variant='small'>$ {productData.price}</Button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
