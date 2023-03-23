3d1bfa3456e4a5b9615ee49b5ebfe3f9574bf76e
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-03-23 10:38:06 +0100

    fix bugs with container

--- src/components/common/Banner/Banner.js
+++ src/components/common/Banner/Banner.js
@@ -4,21 +4,23 @@ import styles from './Banner.module.scss';
 const Banner = () => {
   return (
     <div className={styles.root}>
-      <div className={styles.banner}>
-        <p className={styles.title}>
-          BEDROOM <strong>FURNITURE</strong>
-        </p>
-        <p className={styles.subtitle}>
-          ALWAYS <span>25%</span> OFF OR MORE
-        </p>
-      </div>
-      <div className={styles.menu}>
-        <p>
-          Home &gt;{' '}
-          <span>
-            <strong>Furniture</strong>
-          </span>
-        </p>
+      <div className='container'>
+        <div className={styles.banner}>
+          <p className={styles.title}>
+            BEDROOM <strong>FURNITURE</strong>
+          </p>
+          <p className={styles.subtitle}>
+            ALWAYS <span>25%</span> OFF OR MORE
+          </p>
+        </div>
+        <div className={styles.menu}>
+          <p>
+            Home &gt;{' '}
+            <span>
+              <strong>Furniture</strong>
+            </span>
+          </p>
+        </div>
       </div>
     </div>
   );
