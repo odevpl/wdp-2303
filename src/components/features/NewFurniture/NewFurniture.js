9725cab8642b2cd8d6e6aaae2fd3e8a6f9e11165
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-03-23 10:09:05 +0100

    Resolve conficts

--- src/components/features/NewFurniture/NewFurniture.js
+++ src/components/features/NewFurniture/NewFurniture.js
@@ -2,11 +2,8 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styles from './NewFurniture.module.scss';
 import ProductBox from '../../common/ProductBox/ProductBox';
-<<<<<<< HEAD
 import CompareBar from '../../common/CompareBar/CompareBar';
-=======
 import Swipeable from '../../common/Swipeable/Swipeable';
->>>>>>> 8931fea (Add Swipeable component)
 
 const NewFurniture = ({ categories, products, viewportMode }) => {
   const [activePage, setActivePage] = useState(0);
@@ -29,10 +26,8 @@ const NewFurniture = ({ categories, products, viewportMode }) => {
     }, 600);
   };
 
-<<<<<<< HEAD
   const rows = viewportMode === 'mobile' ? 1 : viewportMode === 'tablet' ? 2 : 8;
   useEffect(() => handlePageChange(0), [viewportMode]);
-=======
   const leftAction = e => {
     e.preventDefault();
     console.log('left Action');
@@ -42,7 +37,6 @@ const NewFurniture = ({ categories, products, viewportMode }) => {
     console.log('right Action');
   };
 
->>>>>>> 8931fea (Add Swipeable component)
   const categoryProducts = products.filter(item => item.category === activeCategory);
   const pagesCount = Math.ceil(categoryProducts.length / rows);
 
@@ -60,91 +54,58 @@ const NewFurniture = ({ categories, products, viewportMode }) => {
     );
   }
 
-<<<<<<< HEAD
   return (
-    <div className={styles.root}>
-      <div className='container'>
-        <div className={styles.panelBar}>
-          <div className='row g-0 align-items-end'>
-            <div className={'col-md-auto col-12 mb-3 mb-md-0 ' + styles.heading}>
-              <h3>New furniture</h3>
-            </div>
-            <div className={'col-md col-12 ' + styles.menu}>
-              <ul>
-                {categories.map(item => (
-                  <li key={item.id}>
-                    <a
-                      className={item.id === activeCategory && styles.active}
-                      onClick={() => handleCategoryChange(item.id)}
-                    >
-                      {item.name}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className={'col-lg-auto col-12 text-center ' + styles.dots}>
-              <ul>{dots}</ul>
-            </div>
-          </div>
-        </div>
-        <div className={`row + ${fade ? styles.fadeIn : styles.fadeOut}`}>
-          {categoryProducts
-            .slice(activePage * rows, (activePage + 1) * rows)
-            .map(item => (
-              <div key={item.id} className='col-lg-3 col-md-6 col-12'>
-                <ProductBox {...item} />
+    <Swipeable leftAction={leftAction} rightAction={rightAction}>
+      <div className={styles.root}>
+        <div className='container'>
+          <div className={styles.panelBar}>
+            <div className='row g-0 align-items-end'>
+              <div className={'col-md-auto col-12 mb-3 mb-md-0 ' + styles.heading}>
+                <h3>New furniture</h3>
               </div>
-            ))}
-        </div>
-        <CompareBar />
-      </div>
-    </div>
-  );
-=======
-    return (
-      <Swipeable leftAction={leftAction} rightAction={rightAction}>
-        <div className={styles.root}>
-          <div className='container'>
-            <div className={styles.panelBar}>
-              <div className='row no-gutters align-items-end'>
-                <div className={'col-auto ' + styles.heading}>
-                  <h3>New furniture</h3>
-                </div>
-                <div className={'col ' + styles.menu}>
-                  <ul>
-                    {categories.map(item => (
-                      <li key={item.id}>
-                        <a
-                          className={item.id === activeCategory && styles.active}
-                          onClick={() => handleCategoryChange(item.id)}
-                        >
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-                <div className={'col-auto ' + styles.dots}>
-                  <ul>{dots}</ul>
-                </div>
+              <div className={'col-md col-12 ' + styles.menu}>
+                <ul>
+                  {categories.map(item => (
+                    <li key={item.id}>
+                      <a
+                        className={item.id === activeCategory && styles.active}
+                        onClick={() => handleCategoryChange(item.id)}
+                      >
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className={'col-lg-auto col-12 text-center ' + styles.dots}>
+                <ul>{dots}</ul>
               </div>
             </div>
             <div className='row'>
               {categoryProducts
-                .slice(activePage * 8, (activePage + 1) * 8)
+                .slice(activePage * rows, (activePage + 1) * rows)
                 .map(item => (
                   <div key={item.id} className='col-lg-3 col-md-6 col-12'>
                     <ProductBox {...item} />
                   </div>
                 ))}
             </div>
+            <CompareBar />
+          </div>
+          <div className={`row + ${fade ? styles.fadeIn : styles.fadeOut}`}>
+            {categoryProducts
+              .slice(activePage * rows, (activePage + 1) * rows)
+              .map(item => (
+                <div key={item.id} className='col-lg-3 col-md-6 col-12'>
+                  <ProductBox {...item} />
+                </div>
+              ))}
           </div>
         </div>
-      </Swipeable>
-    );
-  }
->>>>>>> 8931fea (Add Swipeable component)
+        <CompareBar />
+      </div>
+    </Swipeable>
+  );
 };
 
 NewFurniture.propTypes = {
