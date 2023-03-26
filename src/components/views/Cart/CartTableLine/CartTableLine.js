74d9c5ea5b2dcd954ba4643644045945d585e168
Bartlomiej Pedryc <b.pedryc@poczta.fm>
2023-03-26 10:23:30 +0200

    Add cartBar, main table, button to main page

---
+++ src/components/views/Cart/CartTableLine/CartTableLine.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
+import styles from './CartTableLine.module.scss';
+import PropTypes from 'prop-types';
+
+const CartTableLine = props => {
+  const total = props.price * props.amount;
+
+  return (
+    <div className={`row ${styles.nextRows}`}>
+      <span className='col-8 h-100'>
+        <div className={`row w-100 ${styles.vertCenter}`}>
+          <span className={`col-1 text-center ${styles.vertCenter}`}>
+            <AiOutlineCloseCircle className={styles.tableIcon} />
+          </span>
+          <span className={`col-1 text-center ${styles.vertCenter}`}>
+            <div className={styles.productImage}></div>
+          </span>
+          <span className={`col-10  ps-4 ${styles.vertCenter}`}>
+            {props.productName}
+          </span>
+        </div>
+      </span>
+      <span className={`col-1 text-center ${styles.price}`}>
+        ${props.price.toFixed(2)}
+      </span>
+      <span className='col-2 text-center'>- {props.amount} +</span>
+      <span className={`col-1 text-center ${styles.price}`}>${total.toFixed(2)}</span>
+    </div>
+  );
+};
+
+CartTableLine.propTypes = {
+  productName: PropTypes.string,
+  price: PropTypes.number,
+  amount: PropTypes.number,
+};
+
+export default CartTableLine;
