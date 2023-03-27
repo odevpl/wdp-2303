eb2f83ced9b1655810cff4c958c9cb7894fc0252
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-03-27 17:07:07 +0200

    Add Swipeable wrapper on Feedback component

--- src/components/features/Feedback/Feedback.js
+++ src/components/features/Feedback/Feedback.js
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import styles from './Feedback.module.scss';
 import { useState } from 'react';
 import { getAllFeedbacks } from '../../../redux/feedbacksRedux';
+import Swipeable from '../../common/Swipeable/Swipeable';
 
 const Feedback = () => {
   const [activePage, setActivePage] = useState(0);
@@ -13,55 +14,62 @@ const Feedback = () => {
   };
 
   return (
-    <div className={styles.root}>
-      <div className='container'>
-        <div className={styles.panelBar}>
-          <div className='row g-0 align-items-end'>
-            <div className={'col-10 ' + styles.heading}>
-              <h3>Client Feedback</h3>
+    <>
+      <Swipeable>
+        <div className={styles.root}>
+          <div className='container'>
+            <div className={styles.panelBar}>
+              <div className='row g-0 align-items-end'>
+                <div className={'col-10 ' + styles.heading}>
+                  <h3>Client Feedback</h3>
+                </div>
+                <div className={'col-auto ' + styles.dots}>
+                  <ul>
+                    {feedbacks.map(feedback => (
+                      <li key={feedback.id}>
+                        <a
+                          onClick={() => handlePageChange(feedback.id)}
+                          className={feedback.id === activePage && styles.active}
+                        ></a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
             </div>
-            <div className={'col-auto ' + styles.dots}>
-              <ul>
-                {feedbacks.map(feedback => (
-                  <li key={feedback.id}>
-                    <a
-                      onClick={() => handlePageChange(feedback.id)}
-                      className={feedback.id === activePage && styles.active}
-                    ></a>
-                  </li>
-                ))}
-              </ul>
+            <div className='row justify-content-center'>
+              {feedbacks.slice(activePage, activePage + 1).map(feedback => (
+                <div
+                  key={feedback.id}
+                  className='row justify-content-center text-center'
+                >
+                  <div className={'row justify-content-center ' + styles.header}>
+                    &rdquo;
+                  </div>
+                  <div className='row justify-content-center mx-5 mb-2'>
+                    <p>{feedback.description}</p>
+                  </div>
+                  <div className='row mb-4'>
+                    <div className='col-6'>
+                      <div className={styles.image}>
+                        <img
+                          alt={feedback.author}
+                          src={`${process.env.PUBLIC_URL}/images/feedback/${feedback.id}.jpg`}
+                        />
+                      </div>
+                    </div>
+                    <div className='col-6 text-nowrap text-left'>
+                      <b className='p-0'>{feedback.author}</b>
+                      <p className='p-0'>{feedback.status}</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         </div>
-        <div className='row justify-content-center'>
-          {feedbacks.slice(activePage, activePage + 1).map(feedback => (
-            <div key={feedback.id} className='row justify-content-center text-center'>
-              <div className={'row justify-content-center ' + styles.header}>
-                &rdquo;
-              </div>
-              <div className='row justify-content-center mx-5 mb-2'>
-                <p>{feedback.description}</p>
-              </div>
-              <div className='row mb-4'>
-                <div className={'col-6 ' + styles.photo}>
-                  <div className={styles.image}>
-                    <img
-                      alt={feedback.author}
-                      src={`${process.env.PUBLIC_URL}/images/feedback/${feedback.id}.jpg`}
-                    />
-                  </div>
-                </div>
-                <div className={'col-6 text-nowrap ' + styles.author}>
-                  <b className='p-0'>{feedback.author}</b>
-                  <p className='p-0'>{feedback.status}</p>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
+      </Swipeable>
+    </>
   );
 };
 
