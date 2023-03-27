7d9bd01dca03d4cb9d3ef64c868c6b15698e3966
Lukasz Kryskiewicz <lukasz.kryskiewicz@gmail.com>
2023-03-27 14:40:07 +0200

    Add slider to hotDeals

--- src/components/features/Featured/Featured.js
+++ src/components/features/Featured/Featured.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { getHotDeals } from '../../../redux/productsRedux';
 import styles from './Featured.module.scss';
@@ -6,7 +6,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircle } from '@fortawesome/free-solid-svg-icons';
 import clsx from 'clsx';
 import StarsReview from '../../common/StarsReview/StarsReview';
-
 import {
   faShoppingBasket,
   faChevronRight,
@@ -14,10 +13,24 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import Button from '../../common/Button/Button';
 import ActionButton from '../../common/ActionButton/ActionButton';
+import Carousel from 'react-bootstrap/Carousel';
 
 const Featured = () => {
-  const hotDeals = useSelector(getHotDeals);
-  const hotDeal = hotDeals[0];
+  const [slideInterval, setSlideInterval] = useState(3000);
+  const [index, setIndex] = useState(0);
+  const hotDeals = useSelector(getHotDeals).slice(0, 3); // max 3 elementy ustawia obecnie
+
+  const handleSelect = selectedIndex => {
+    setIndex(selectedIndex);
+  };
+
+  const onClickHandler = index => {
+    setIndex(index);
+    setSlideInterval(7000);
+    setTimeout(() => {
+      setSlideInterval(3000);
+    }, 7000);
+  };
 
   return (
     <div className={styles.root}>
@@ -34,110 +47,121 @@ const Featured = () => {
             >
               <h4 className='m-0 p-3'>HOT DEALS</h4>
               <div className={clsx(styles.icons, 'm-2')}>
-                <FontAwesomeIcon
-                  className={clsx(styles.icon, 'me-1')}
-                  icon={faCircle}
-                />
-                <FontAwesomeIcon
-                  className={clsx(styles.icon, 'me-1')}
-                  icon={faCircle}
-                />
-                <FontAwesomeIcon
-                  className={clsx(styles.icon, 'me-1')}
-                  icon={faCircle}
-                />
+                {hotDeals.map((hotDeal, i) => (
+                  <FontAwesomeIcon
+                    key={i}
+                    className={clsx(
+                      styles.icon,
+                      'me-1',
+                      index === i ? styles.active : null
+                    )}
+                    icon={faCircle}
+                    onClick={() => onClickHandler(i)}
+                  />
+                ))}
               </div>
             </div>
 
             <div className={styles.productBox}>
-              <div className={styles.photo}>
-                <div className={styles.image}>
-                  <img
-                    alt={hotDeal.name}
-                    src={`${process.env.PUBLIC_URL}/images/furniture/${hotDeal.category}/${hotDeal.id}.jpg`}
-                  />
-                </div>
+              <Carousel
+                activeIndex={index}
+                onSelect={handleSelect}
+                interval={slideInterval}
+                pause={false}
+              >
+                {hotDeals.map(hotDeal => (
+                  <Carousel.Item key={hotDeal.name}>
+                    <div className={styles.photo}>
+                      <div className={styles.image}>
+                        <img
+                          alt={hotDeal.name}
+                          src={`${process.env.PUBLIC_URL}/images/furniture/${hotDeal.category}/${hotDeal.id}.jpg`}
+                        />
+                      </div>
 
-                <div className={styles.button}>
-                  <Button variant='small'>
-                    <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO
-                    CART
-                  </Button>
-                </div>
-                <div className={`${styles.timer} row`}>
-                  <li className='col'>
-                    <FontAwesomeIcon
-                      className={`${styles.iconTimer} me-1`}
-                      icon={faCircle}
-                    />
-                    <p>
-                      25
-                      <br />
-                      DAYS
-                    </p>
-                  </li>
-                  <li className='col'>
-                    <FontAwesomeIcon
-                      className={`${styles.iconTimer} me-1`}
-                      icon={faCircle}
-                    />
-                    <p>
-                      10<br></br>HRS
-                    </p>
-                  </li>
-                  <li className='col'>
-                    <FontAwesomeIcon
-                      className={`${styles.iconTimer} me-1`}
-                      icon={faCircle}
-                    />
-                    <p>
-                      45<br></br>MIN
-                    </p>
-                  </li>
-                  <li className='col'>
-                    <FontAwesomeIcon
-                      className={`${styles.iconTimer} me-1`}
-                      icon={faCircle}
-                    />
-                    <p>
-                      30<br></br>SEC
-                    </p>
-                  </li>
-                </div>
-              </div>
-              <div className={styles.content}>
-                <h5>{hotDeal.name}</h5>
-                <StarsReview
-                  id={hotDeal.id}
-                  stars={hotDeal.stars}
-                  myStars={hotDeal.myStars}
-                  name={hotDeal.name}
-                />
-              </div>
-              <div className={styles.line}></div>
-              <div className={styles.actions}>
-                <div className={styles.outlines}>
-                  <ActionButton
-                    id={hotDeal.id}
-                    favourite={hotDeal.favourite}
-                    buttonType={'favourite'}
-                  />
-                  <ActionButton
-                    id={hotDeal.id}
-                    compare={hotDeal.compare}
-                    buttonType={'compare'}
-                  />
-                  <ActionButton id={hotDeal.id} buttonType={'quickView'} />
-                </div>
-                <div className={styles.prices}>
-                  {hotDeal.oldPrice && (
-                    <div className={styles.oldPrice}>$ {hotDeal.oldPrice}</div>
-                  )}
-                  <div className={styles.price}>
-                    <Button variant='small'>$ {hotDeal.price}</Button>
-                  </div>
-                </div>
-              </div>
+                      <div className={styles.button}>
+                        <Button variant='small'>
+                          <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon>{' '}
+                          ADD TO CART
+                        </Button>
+                      </div>
+                      <div className={`${styles.timer} row`}>
+                        <li className='col'>
+                          <FontAwesomeIcon
+                            className={`${styles.iconTimer} me-1`}
+                            icon={faCircle}
+                          />
+                          <p>
+                            25
+                            <br />
+                            DAYS
+                          </p>
+                        </li>
+                        <li className='col'>
+                          <FontAwesomeIcon
+                            className={`${styles.iconTimer} me-1`}
+                            icon={faCircle}
+                          />
+                          <p>
+                            10<br></br>HRS
+                          </p>
+                        </li>
+                        <li className='col'>
+                          <FontAwesomeIcon
+                            className={`${styles.iconTimer} me-1`}
+                            icon={faCircle}
+                          />
+                          <p>
+                            45<br></br>MIN
+                          </p>
+                        </li>
+                        <li className='col'>
+                          <FontAwesomeIcon
+                            className={`${styles.iconTimer} me-1`}
+                            icon={faCircle}
+                          />
+                          <p>
+                            30<br></br>SEC
+                          </p>
+                        </li>
+                      </div>
+                    </div>
+                    <div className={styles.content}>
+                      <h5>{hotDeal.name}</h5>
+                      <StarsReview
+                        id={hotDeal.id}
+                        stars={hotDeal.stars}
+                        myStars={hotDeal.myStars}
+                        name={hotDeal.name}
+                      />
+                    </div>
+                    <div className={styles.line}></div>
+                    <div className={styles.actions}>
+                      <div className={styles.outlines}>
+                        <ActionButton
+                          id={hotDeal.id}
+                          favourite={hotDeal.favourite}
+                          buttonType={'favourite'}
+                        />
+                        <ActionButton
+                          id={hotDeal.id}
+                          compare={hotDeal.compare}
+                          buttonType={'compare'}
+                        />
+                        <ActionButton id={hotDeal.id} buttonType={'quickView'} />
+                      </div>
+                      <div className={styles.prices}>
+                        {hotDeal.oldPrice && (
+                          <div className={styles.oldPrice}>$ {hotDeal.oldPrice}</div>
+                        )}
+                        <div className={styles.price}>
+                          <Button variant='small'>$ {hotDeal.price}</Button>
+                        </div>
+                      </div>
+                    </div>
+                  </Carousel.Item>
+                ))}
+              </Carousel>
             </div>
           </div>
 
