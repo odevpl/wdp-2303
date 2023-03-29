aeff9b98c4c8a357eb20cb56a269f12e0f277105
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-03-29 13:31:26 +0200

    Add FilterByCategory component to ProductList

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -3,6 +3,7 @@ import FilterBySize from '../../common/FilterBySize/FilterBySize';
 // import PropTypes from 'prop-types';
 import styles from './ProductList.module.scss';
 import Brands from '../../layout/Brands/Brands';
+import FilterByCategory from '../../common/FilterCategory/FilteByCategory';
 
 const ProductList = () => (
   <div className={styles.root}>
@@ -12,6 +13,7 @@ const ProductList = () => (
         <div className={`col-9 ${styles.productList}`}>This is Product List</div>
         <div className={`col-3 ${styles.filters}`}>
           These are filters
+          <FilterByCategory></FilterByCategory>
           <FilterBySize></FilterBySize>
         </div>
       </div>
