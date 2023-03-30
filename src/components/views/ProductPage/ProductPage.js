526caf6b8c64292087e35da003687c24dfa50129
Szymek131 <sz.wiktor131@gmail.com>
2023-03-30 18:44:12 +0200

    fixed bugs

--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
-import Banner from '../../common/Banner/Banner';
-// import PropTypes from 'prop-types';
 import styles from './ProductPage.module.scss';
+import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
 import ProductReview from '../../features/ProductReview/ProductReview';
 
 const ProductPage = () => {
   return (
     <div className={styles.root}>
-      <NewFurniture />
       <ProductReview />
+      <NewFurniture />
     </div>
   );
 };
