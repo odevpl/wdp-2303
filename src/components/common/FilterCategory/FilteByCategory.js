1027a5318ca83f3cd066849c021ce23519c6ee58
Lukasz Kryskiewicz <lukasz.kryskiewicz@gmail.com>
2023-04-02 00:27:01 +0200

    Add filters funcionality

--- src/components/common/FilterCategory/FilteByCategory.js
+++ src/components/common/FilterCategory/FilteByCategory.js
@@ -3,19 +3,23 @@ import styles from './FilterByCategory.module.scss';
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useDispatch } from 'react-redux';
-import { updateFilter } from '../../../redux/filterRedux';
+import { removeFilter, updateFilter } from '../../../redux/filterRedux';
 import { getAll } from '../../../redux/categoriesRedux';
 import { useSelector } from 'react-redux';
 
 const FilterByCategory = () => {
   const dispatch = useDispatch();
   const [activeCategory, setActiveCategory] = useState(null);
-
   const categoryArray = useSelector(state => getAll(state));
 
   const handleClick = category => {
-    setActiveCategory(category);
-    dispatch(updateFilter({ name: 'categoryFilter', value: category }));
+    if (category.name !== activeCategory) {
+      setActiveCategory(category.name);
+      dispatch(updateFilter({ name: 'categoryFilter', value: category.id }));
+    } else {
+      setActiveCategory(null);
+      dispatch(removeFilter({ name: 'categoryFilter' }));
+    }
   };
   return (
     <>
@@ -29,7 +33,7 @@ const FilterByCategory = () => {
                   key={category.name}
                   className={`d-flex align-items-center + ${activeCategory ===
                     category.name && styles.active}`}
-                  onClick={() => handleClick(category.name)}
+                  onClick={() => handleClick(category)}
                 >
                   <FontAwesomeIcon icon={faAngleRight} />
                   <h4>{category.name}</h4>
