61bdd5d0d29f6de404979e88bb590f860a0da4f7
Szymek131 <sz.wiktor131@gmail.com>
2023-04-03 16:53:19 +0200

    add simple chatBot functionality

--- src/components/common/ChatBot/ChatBot.js
+++ src/components/common/ChatBot/ChatBot.js
@@ -12,13 +12,32 @@ import {
   faComments,
 } from '@fortawesome/free-solid-svg-icons';
 import { Form } from 'react-bootstrap';
+import { set } from 'react-hook-form';
 
 const ChatBot = () => {
   const [activeChat, setActiveChat] = useState(false);
+  const [userInput, setUserInput] = useState('');
+  const [chatBotAnswer, setChatBotAnswer] = useState('How can I help you?');
+
+  const workHoursAnswer =
+    'Firma pracuje od poniedziałku do piątku w godzinach od 8:00 do 16:00';
+  const deliveryCostAnswer =
+    'Koszt przesyłki jest zależny od gabarytów. Zwykle jest to 200zł za dostawę.';
 
   const handleClick = e => {
     e.preventDefault();
     setActiveChat(!activeChat);
+  };
+
+  const handleOnSubmit = e => {
+    e.preventDefault();
+    setChatBotAnswer('');
+    if (userInput.includes('godziny pracy') || userInput.includes('czas pracy')) {
+      setChatBotAnswer(workHoursAnswer);
+    } else if (userInput.includes('koszt przesyłki')) {
+      setChatBotAnswer(deliveryCostAnswer);
+    }
+    setUserInput('');
   };
 
   return (
@@ -44,7 +63,7 @@ const ChatBot = () => {
                     <FontAwesomeIcon icon={faRobot}></FontAwesomeIcon>
                   </div>
                   <div className={clsx('col-10 p-2 my-2', styles.message)}>
-                    <p className='m-0'>How can I help you?</p>
+                    <p className='m-0'>{chatBotAnswer}</p>
                   </div>
                   <div className={clsx('col-12 py-2 my-2', styles.topic)}>
                     <p className='m-0'>Choose topic</p>
@@ -61,9 +80,10 @@ const ChatBot = () => {
                       as='textarea'
                       rows={1}
                       placeholder='Write your question'
+                      onChange={e => setUserInput(e.target.value)}
                     />
                   </Form.Group>
-                  <Button type='submit' className='col-1 p-0'>
+                  <Button className='col-1 p-0' onClick={handleOnSubmit}>
                     <FontAwesomeIcon
                       className='p-2'
                       icon={faPaperPlane}
